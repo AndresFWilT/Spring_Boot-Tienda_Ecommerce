e9c3e1270bd22550e26edb152bc83eba44514f3b
Andres Felipe Wilches Torres <andresfwilchest@gmail.com>
2021-10-14 20:27:33 -0500

    Final

--- src/main/java/com/miempresa/aplicacion/controladores/ControladorFactura.java
+++ src/main/java/com/miempresa/aplicacion/controladores/ControladorFactura.java
@@ -17,21 +17,25 @@ import org.springframework.web.servlet.view.RedirectView;
 import lombok.extern.slf4j.Slf4j;
 import com.miempresa.aplicacion.modelos.ProductoDAO;
 import com.miempresa.aplicacion.modelos.VendedorDAO;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor(onConstructor = @__(
         @Autowired))
 public class ControladorFactura {
-    
+
     @Autowired
     private final FacturaDAO repositorioFactura;
     private final ProductoDAO repositorioProducto;
     private final VendedorDAO repositorioVendedor;
+    
+    private String factura,producto,vendedor,fecha,id;
 
     @GetMapping("/facturas") //path del controlador
     public String getTodasLasFacturas(Model model) {
         Iterable<Factura> facturas = repositorioFactura.findAll();
+        log.info("" + model.toString());
         model.addAttribute("facturas", facturas);
         return "vistaFactura";
     }
@@ -51,53 +55,128 @@ public class ControladorFactura {
         return "vistaCrearFactura";
     }
     
+    @PostMapping("/crear/factura")
+    public RedirectView procesarProducto(@ModelAttribute FacturaDto facturaDto,RedirectAttributes attribute) {
+        //Se inicializan variables
+        Producto producto;
+        Vendedor vendedor;
+        try {
+            //Primero se llama a los DAO de producto y vendedor
+            producto = repositorioProducto.findByCodProducto(facturaDto.getCodigoProducto());
+            vendedor = repositorioVendedor.findByCodVendedor(facturaDto.getCodigoVendedor());
+            //Se inicializa factura
+            Factura factura = new Factura();
+            //Se empieza a setear las columnas de la tabla factura en funcion de los datos que se recibieron y
+            //tambien en funcion de lo que se obtenga segun el codigo producto o no.
+            factura.setNumeroFactura(facturaDto.getNumeroFactura());
+            factura.setProducto(producto);
+            factura.setVendedor(vendedor);
+            factura.setFechaVenta(facturaDto.getFechaVenta());
+            //Se trae el precio del producto
+            //Se crea una variable para obtener el precio producto del modelo
+            Double f = producto.getPrecioProducto();
+            float precio = f.floatValue();
+            //Luego se implementa esa variable a la factura
+            factura.setValorFactura(precio);
+            //Query para guardar datos 
+            Factura facturaGuardada = repositorioFactura.save(factura);
+            if (facturaGuardada == null) {
+                attribute.addFlashAttribute("error", "No agregaste datos");
+                log.info("No se pudieron agregar datos");
+                return new RedirectView("/crear/factura/", true);
+            }
+            log.info("Datos añadidos");
+            return new RedirectView("/facturas", true);
+        } catch (Exception e) {
+            attribute.addFlashAttribute("error", "No se pudieron agregar datos, revisa los codigos de producto,vendedor y llena todos los campos");
+            log.info("No se pudo realizar la transaccion, ya que no encuentra un producto o un vendedor");
+            return new RedirectView("/crear/factura", true);
+        }
+    }
+
     //Editar es para eliminar o actulizar
     @GetMapping("/editar/factura")
-    public String editarFactura(Model model){
+    public String editarFactura(Model model) {
         Iterable<Factura> facturas = repositorioFactura.findAll();
         model.addAttribute("facturas", facturas);
         return "vistaEditarFactura";
     }
-    
+
     @GetMapping("/actualizar/factura") //path del controlador
-    public String actualizarFactura(Model model) {
-        model.addAttribute("factura", new FacturaDto());
+    public String actualizarFactura(@ModelAttribute FacturaDto facturaDto,Factura factura,Model model) {
+        log.info("numero de factura: "+factura.getIdVenta());
+        this.id = String.valueOf(factura.getIdVenta());
+        Factura f = repositorioFactura.findByIdVenta(factura.getIdVenta());
+        //Se setea el DTO como modelo para poder editar
+        facturaDto.setCodigoProducto(f.getProducto().getCodProducto());
+        facturaDto.setCodigoVendedor(f.getVendedor().getCodVendedor());
+        facturaDto.setFechaVenta(f.getFechaVenta());
+        facturaDto.setIdVenta(f.getIdVenta());
+        facturaDto.setNumeroFactura(f.getNumeroFactura());
+        facturaDto.setValorFactura(f.getValorFactura());
+        log.info(""+facturaDto);
+        //Se agrega el DTO al modelo de la vista actualizar factura
+        model.addAttribute("facturaDto", facturaDto);
         return "vistaActualizarFactura";
     }
-    
+
     @PostMapping("/actualizar/factura")
-    public RedirectView editarProducto(Factura factura){
-        Long idVenta = factura.getIdVenta();
-        factura = repositorioFactura.findByIdVenta(idVenta);
-        log.info("numero de factura seleccionado "+factura);
-        Factura f = repositorioFactura.findByIdVenta(idVenta);
-        return new RedirectView("redirect:/vistaFactura",true);
+    public RedirectView editarProducto(FacturaDto facturaDto, RedirectAttributes attribute) {
+        try {
+            //Validaciones basicas
+            //Que no esten vacios los campos
+            if (facturaDto.getNumeroFactura().equals("") || facturaDto.getNumeroFactura().equals(null) || facturaDto.getNumeroFactura().equals(" ")) {
+                log.info("codigo de factura nulo");
+                attribute.addFlashAttribute("error", "Complete el campo de codigo producto");
+                return new RedirectView("redirect:../actualizar/factura?idVenta=" + this.id, true);
+            } else if (facturaDto.getCodigoProducto().equals("") || facturaDto.getCodigoProducto().equals(null) || facturaDto.getCodigoProducto().equals(" ")) {
+                log.info("codigo del producto nulo");
+                attribute.addFlashAttribute("error", "Complete el campo de codigo del producto");
+                return new RedirectView("redirect:../actualizar/factura?idVenta=" + this.id, true);
+            }else if (facturaDto.getCodigoVendedor().equals("") || facturaDto.getCodigoVendedor().equals(null) || facturaDto.getCodigoVendedor().equals(" ")) {
+                log.info("codigo del vendedor nulo");
+                attribute.addFlashAttribute("error", "Complete el campo de codigo de vendedor");
+                return new RedirectView("redirect:../actualizar/factura?idVenta=" + this.id, true);
+            }
+            //Que exista el valor al cual se va a actualizar el cod producto-vendedor
+            //Para producto
+            Producto p = repositorioProducto.findByCodProducto(facturaDto.getCodigoProducto());
+            Vendedor v = repositorioVendedor.findByCodVendedor(facturaDto.getCodigoVendedor());
+            if(String.valueOf(p).equals(null)||String.valueOf(p).equals("null")){
+                log.info("codigo del producto no valido");
+                attribute.addFlashAttribute("atencion", "El codigo de productos que diligencio, no se encuentra registrado");
+                return new RedirectView("redirect:../actualizar/factura?idVenta=" + this.id, true);
+            //Para producto
+            }else if(String.valueOf(v).equals(null)||String.valueOf(v).equals("null")){
+                log.info("codigo del vendedor no valido");
+                attribute.addFlashAttribute("atencion", "El codigo de vendedor que diligencio, no se encuentra registrado");
+                return new RedirectView("redirect:../actualizar/factura?idVenta=" + this.id, true);
+            }
+            //Luego de hacer validaciones, se pasa a editar
+            Factura f = repositorioFactura.findByIdVenta(facturaDto.getIdVenta());
+            //Se setean las variables del modelo que se recibe de la vista (hayan cambios o no)
+            //set de elementos a cambiar
+            f.setFechaVenta(facturaDto.getFechaVenta());
+            f.setNumeroFactura(facturaDto.getNumeroFactura());
+            f.setProducto(p);
+            f.setVendedor(v);
+            //Se hace el update
+            Factura facturaGuardada = repositorioFactura.save(f);
+            if (facturaGuardada == null) {
+                attribute.addFlashAttribute("error", "No se pudo guardar la factura");
+                return new RedirectView("../actualizar/factura?idVenta=" + this.id, true);
+            }
+            return new RedirectView("../facturas", true);
+        } catch (Exception e) {
+            return new RedirectView("redirect:../", true);
+        }
     }
 
-    @PostMapping("/crear/factura")
-    public RedirectView procesarProducto(@ModelAttribute FacturaDto facturaDto) {
-        Producto producto = repositorioProducto.findByCodProducto(facturaDto.getCodigoProducto());
-        Vendedor vendedor = repositorioVendedor.findByCodVendedor(facturaDto.getCodigoVendedor());
-        Factura factura = new Factura();
-        factura.setNumeroFactura(facturaDto.getNumeroFactura());
-        factura.setProducto(producto);
-        factura.setVendedor(vendedor);
-        factura.setFechaVenta(facturaDto.getFechaVenta());
-        factura.setValorFactura(facturaDto.getValorFactura());
-        Factura facturaGuardada = repositorioFactura.save(factura);
-       if (facturaGuardada == null){
-           log.info("No se pudieron agregar datos");
-           return new RedirectView("/crear/factura/",true);
-       }
-       log.info("Datos añadidos");
-       return new RedirectView("/facturas/",true);
-    }
-  
     @GetMapping("/eliminar/factura")
-    public String eliminarProducto(Factura factura){
+    public String eliminarProducto(Factura factura) {
         Long idVenta = factura.getIdVenta();
         factura = repositorioFactura.findByIdVenta(idVenta);
-        log.info("numero de factura seleccionado "+factura);
+        log.info("numero de factura seleccionado " + factura);
         repositorioFactura.delete(factura);
         return "redirect:/editar/factura";
     }
--- src/main/java/com/miempresa/aplicacion/controladores/ControladorProducto.java
+++ src/main/java/com/miempresa/aplicacion/controladores/ControladorProducto.java
@@ -1,12 +1,11 @@
 package com.miempresa.aplicacion.controladores;
 
 import com.miempresa.aplicacion.dtos.ProductoDto;
-import com.miempresa.aplicacion.modelos.Factura;
 import com.miempresa.aplicacion.modelos.Producto;
+import com.miempresa.aplicacion.dtos.FacturaDto;
+import com.miempresa.aplicacion.modelos.Factura;
 import java.util.ArrayList;
 import java.util.List;
-
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,75 +17,143 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 import com.miempresa.aplicacion.modelos.ProductoDAO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RequiredArgsConstructor(onConstructor = @__(
+        @Autowired))
 public class ControladorProducto {
-    
+
     private final ProductoDAO repositorioProducto;
+    private String codPro,nombre,descripcion,precio;
     
+
     @GetMapping("/productos") //path del controlador
-    public String getTodosLosProductos(Model model){
+    public String getTodosLosProductos(Model model) {
         Iterable<Producto> productos = repositorioProducto.findAll();
-        model.addAttribute("productos",productos);
+        model.addAttribute("productos", productos);
         return "vistaProducto";
-    }    
-    
+    }
+
     @GetMapping("/productos/{codigoProducto}") //path del controlador
-    public String getProductoById(@PathVariable String codigoProducto, Model model){
+    public String getProductoById(@PathVariable String codigoProducto, Model model) {
         List<String> listaProducto = new ArrayList<>();
         listaProducto.add(codigoProducto);
         Iterable<Producto> productos = repositorioProducto.findAllById(listaProducto);
-        model.addAttribute("productos",productos);
+        model.addAttribute("productos", productos);
         return "vistaProducto";
     }
- 
+
     @GetMapping("/crear/producto") //path del controlador
-    public String crearProducto(Model model){
-        
-        model.addAttribute("producto",new Producto());
+    public String crearProducto(Model model) {
+
+        model.addAttribute("producto", new Producto());
         return "vistaCrearProducto";
-    }   
-    
+    }
+
     @PostMapping("/crear/producto")
-    public RedirectView procesarProducto(@ModelAttribute Producto producto){
-       Producto productoGuardado = repositorioProducto.save(producto);
-       if (productoGuardado == null){
-           return new RedirectView("/crear/producto/",true);
-       }
-       return new RedirectView("/productos/",true);
-    }    
-    
+    public RedirectView procesarProducto(@ModelAttribute Producto producto, RedirectAttributes attribute) {
+        try {
+            log.info("tipo: " + ((Object) producto.getPrecioProducto()).getClass().getSimpleName() + "valor: " + producto.getPrecioProducto());
+            //Validaciones de vacio
+            if (producto.getCodProducto().equals(null) || producto.getCodProducto().equals("")) {
+                attribute.addFlashAttribute("error", "No agregaste el codigo del producto");
+                return new RedirectView("/crear/producto/", true);
+            } else if (producto.getNombreProducto().equals(null) || producto.getNombreProducto().equals("")) {
+                attribute.addFlashAttribute("error", "No agregaste el nombre del producto");
+                return new RedirectView("/crear/producto/", true);
+            } else if (producto.getDescripcionProducto().equals(null) || producto.getDescripcionProducto().equals("")) {
+                attribute.addFlashAttribute("error", "No agregaste una descripcion del producto");
+                return new RedirectView("/crear/producto/", true);
+            } else if (producto.getPrecioProducto() <= 0.0) {
+                attribute.addFlashAttribute("error", "No agregaste una valor valido al producto");
+                return new RedirectView("/crear/producto/", true);
+            }
+            Producto productoGuardado = repositorioProducto.save(producto);
+            if (productoGuardado == null) {
+                return new RedirectView("/crear/producto/", true);
+            }
+            return new RedirectView("/productos/", true);
+        } catch (Exception e) {
+            attribute.addFlashAttribute("error", "No se pudieron agregar productos, revisa los campos");
+            return new RedirectView("/crear/producto/", true);
+        }
+
+    }
+
     //Editar es para eliminar o actulizar
     @GetMapping("/editar/producto")
-    public String editarProducto(Model model){
+    public String editarProducto(Model model) {
         Iterable<Producto> productos = repositorioProducto.findAll();
         model.addAttribute("productos", productos);
         return "vistaEditarProducto";
     }
-    
+
     @GetMapping("/actualizar/producto") //path del controlador
-    public String actualizarProducto(Model model) {
-        model.addAttribute("producto", new ProductoDto());
+    public String actualizarProducto(Producto producto, Model model) {
+        log.info(producto.getCodProducto());
+        this.codPro = producto.getCodProducto();
+        Producto p = repositorioProducto.findByCodProducto(this.codPro);
+        model.addAttribute("producto", p);
         return "vistaActualizarProducto";
     }
-    
+
     @PostMapping("/actualizar/producto")
-    public RedirectView editarProducto(Producto producto){
-        String codProducto = producto.getCodProducto();
-        producto = repositorioProducto.findByCodProducto(codProducto);
-        log.info("producto seleccionado "+producto);
-        Producto p = repositorioProducto.findByCodProducto(codProducto);
-        return new RedirectView("redirect:/vistaProducto",true);
+    public RedirectView editarProducto(Producto producto, RedirectAttributes attribute) {
+        try {
+            //Validaciones basicas
+            if (producto.getCodProducto().equals("") || producto.getCodProducto().equals(null) || producto.getCodProducto().equals(" ")) {
+                log.info("codigo de producto nulo");
+                attribute.addFlashAttribute("error", "Complete el campo de codigo producto");
+                return new RedirectView("redirect:../actualizar/producto?codProducto=" + this.codPro, true);
+            } else if (producto.getNombreProducto().equals("") || producto.getNombreProducto().equals(null) || producto.getNombreProducto().equals(" ")) {
+                log.info("nombre del producto nulo");
+                attribute.addFlashAttribute("error", "Complete el campo de nombre del producto");
+                return new RedirectView("../actualizar/producto?codProducto=" + this.codPro, true);
+            }else if (producto.getDescripcionProducto().equals("") || producto.getDescripcionProducto().equals(null) || producto.getDescripcionProducto().equals(" ")) {
+                log.info("descripcion del producto nulo");
+                attribute.addFlashAttribute("error", "Complete el campo de descripcion del producto");
+                return new RedirectView("../actualizar/producto?codProducto=" + this.codPro, true);
+            }else if (producto.getPrecioProducto().equals("") || producto.getPrecioProducto().equals(null) || producto.getPrecioProducto().equals("0.0")||producto.getPrecioProducto()<=0) {
+                log.info("precio del producto nulo");
+                attribute.addFlashAttribute("error", "Complete/Que sea valido el campo de precio del producto");
+                return new RedirectView("../actualizar/producto?codProducto=" + this.codPro, true);
+            }
+            //Luego de hacer validaciones, se pasa a editar
+            Producto p = repositorioProducto.findByCodProducto(codPro);
+            //Se setean las variables del modelo que se recibe de la vista (hayan cambios o no)
+            //Variables globales
+            nombre = producto.getNombreProducto();
+            descripcion = producto.getDescripcionProducto();
+            precio = String.valueOf(producto.getPrecioProducto());
+            //set de elementos a cambiar
+            p.setDescripcionProducto(descripcion);
+            p.setNombreProducto(nombre);
+            p.setPrecioProducto(Double.parseDouble(precio));
+            //Se hace el update
+            Producto productoGuardado = repositorioProducto.save(p);
+            if (productoGuardado == null) {
+                attribute.addFlashAttribute("error", "No se pudo guardar el producto");
+                return new RedirectView("../actualizar/producto?codProducto=" + this.codPro, true);
+            }
+            return new RedirectView("../productos", true);
+        } catch (Exception e) {
+            return new RedirectView("redirect:../", true);
+        }
     }
-    
+
     @GetMapping("/eliminar/producto")
-    public String eliminarProducto(Producto producto){
+    public String eliminarProducto(Producto producto, RedirectAttributes attribute) {
         String codProducto = producto.getCodProducto();
         producto = repositorioProducto.findByCodProducto(codProducto);
-        log.info("producto seleccionado "+producto);
-        repositorioProducto.delete(producto);
+        log.info("producto seleccionado " + producto);
+        try {
+            repositorioProducto.delete(producto);
+            attribute.addFlashAttribute("success", "Producto eliminado");
+        } catch (Exception e) {
+            attribute.addFlashAttribute("error", "No se pudo borrar el producto, ya que se encuentra en las facturas");
+        }
         return "redirect:/editar/producto";
     }
 }
